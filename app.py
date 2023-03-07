d4f4ff3019dc2d3ba73df09df129b5a75e0bcc6f
diawando <abduldiawade777@gmail.com>
2023-03-07 23:56:52 +0000

    creation d'un modèle student

--- app.py
+++ app.py
@@ -2,10 +2,23 @@ import os
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 
-from sqlalchemy .sql import func
+from sqlalchemy import func
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:lqvie@localhost:5433/student'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+class Student(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    age = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime(timezone=True),
+                           server_default= func.now())
+    bio = db.Column(db.Text)
+    
+    def __repr__(self):
+        return f'<Student {self.firstname} >'
